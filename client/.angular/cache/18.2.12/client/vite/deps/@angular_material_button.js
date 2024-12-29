bfac0363fbe9212eec9248ece56eff23c20488b5
eldeebboos <eldeeb_boos@yahoo.com>
2024-12-29 10:23:51 +0200

    Add Initial App

---
+++ client/.angular/cache/18.2.12/client/vite/deps/@angular_material_button.js
@@ -0,0 +1,34 @@
+import {
+  MAT_BUTTON_CONFIG,
+  MAT_FAB_DEFAULT_OPTIONS,
+  MAT_FAB_DEFAULT_OPTIONS_FACTORY,
+  MatAnchor,
+  MatButton,
+  MatButtonModule,
+  MatFabAnchor,
+  MatFabButton,
+  MatIconAnchor,
+  MatIconButton,
+  MatMiniFabAnchor,
+  MatMiniFabButton
+} from "./chunk-FDLTWMD5.js";
+import "./chunk-DP5KAB6J.js";
+import "./chunk-HG5Q2TVL.js";
+import "./chunk-767RW5IN.js";
+import "./chunk-UDMCY33K.js";
+import "./chunk-KBUIKKCC.js";
+export {
+  MAT_BUTTON_CONFIG,
+  MAT_FAB_DEFAULT_OPTIONS,
+  MAT_FAB_DEFAULT_OPTIONS_FACTORY,
+  MatAnchor,
+  MatButton,
+  MatButtonModule,
+  MatFabAnchor,
+  MatFabButton,
+  MatIconAnchor,
+  MatIconButton,
+  MatMiniFabAnchor,
+  MatMiniFabButton
+};
+//# sourceMappingURL=@angular_material_button.js.map
